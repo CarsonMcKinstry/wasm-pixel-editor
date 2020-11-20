75425a38f5622b606767294a629c2f325d3c9ba2
Carson McKinstry <carson.mckinstry@trivago.com>
2020-11-20 16:00:57 +0100

    improve panning

--- web/src/canvas/interactions.ts
+++ web/src/canvas/interactions.ts
@@ -1,28 +1,42 @@
 import { Canvas } from "./Canvas";
+import { clamp } from "lodash";
 
 export const zoom = (canvas: Canvas) => (event: WheelEvent) => {
-    event.preventDefault();
-    const { deltaY } = event;
+  event.preventDefault();
 
-    if (!Number.isInteger(deltaY)) {
-        canvas.scale -= 0.005 * deltaY;
-        canvas.origin = canvas.getOrigin();
-    }
-}
+  const { clientX, clientY, deltaY } = event;
+  if (!Number.isInteger(deltaY)) {
+    const { scale, origin } = canvas;
+
+    const wheel = deltaY < 0 ? 1 : -1;
+
+    const zoom = Math.exp(wheel * 0.05);
+
+    canvas.scale = scale * zoom;
+  }
+};
 
 export const pan = (canvas: Canvas) => (event: WheelEvent) => {
-    event.preventDefault();
-    const { deltaX, deltaY } = event;
+  event.preventDefault();
+  const { deltaX, deltaY } = event;
 
-    if (Number.isInteger(deltaY)) {
-        canvas.origin.x -= deltaX;
-        canvas.origin.y -= deltaY;
-    }
-}
+  if (Number.isInteger(deltaY)) {
+    canvas.origin.x = clamp(
+      canvas.origin.x - deltaX,
+      0 - canvas.trueSize.width,
+      canvas.canvas.width
+    );
+    canvas.origin.y = clamp(
+      canvas.origin.y - deltaY,
+      0 - canvas.trueSize.height,
+      canvas.canvas.height
+    );
+  }
+};
 
 export const handleWindowResize = (canvas: Canvas) => () => {
-    canvas.canvas.width = window.innerWidth;
-    canvas.canvas.height = window.innerHeight;
+  canvas.canvas.width = window.innerWidth;
+  canvas.canvas.height = window.innerHeight;
 
-    canvas.origin = canvas.getOrigin();
-}
+  canvas.origin = canvas.getOrigin();
+};
